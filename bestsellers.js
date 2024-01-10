dce36735458084ca0425ad49e68181a994d6ce5f
DanielDataRocks <127222779+DanielDataRocks@users.noreply.github.com>
2024-01-10 14:35:40 +0100

    Update bestsellers.js

--- bestsellers.js
+++ bestsellers.js
@@ -1,44 +1,21 @@
-// Wersja zmodyfikowana
-// orginalna znajduje się tutaj:
-// https://github.com/google/low_volume_skus/blob/main/low_volume_skus.js
-// Instrukcja:
-// https://services.google.com/fh/files/events/_new_implementation_guide_low_volume_skus.pdf
-//
+// <----------------------------------------------------------USTAWIENIA---------------------------------------------------------->
+//BEST_SELLERY 
+
 
 // wybierz numer "custom label" [0-4] - powinna być całkowicie wolna, bo powodje nadpisanie wszystkich wartości
 // numer powinien być zgodny z plikeiem z Google spreadsheet - kolumna B1 = 'custom_label4'
 var CUSTOM_LABEL_NR = '4';
 
-// Poniżej nalezy podać adres do pliku w Google spreadsheet
-// Nazwy zakładek muszą być takie jak w pliku:
-// https://docs.google.com/spreadsheets/d/1VdzaWRbDF6YtJd-LSnPvObPzoiJsRE_4Hhwx6oJnpy8/edit?usp=sharing
-// Podstawowe nazwy arkuszy to: Data, ALL, Produkty RAMPED_UP
-// Plik można skopiować i będzie działać :) 
+// EXCEL
 var SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1dQa2TyWgXDFNBFo00abXCVdQdbsvisfL229kZykkEek/edit?usp=sharing';
 
+// NAZWA CUSTOM LABEL
+var LABEL_BESTSELLERS = 'bestseller_conversion_last_30d';
 
-// Set the value for the label for newly flagged low volume products.
-// etykieta dla produktów Zombie - można zmienić sobie nazwę.
-var LABEL_LOW = 'low_clicks_last_30d';
-// Set the value for the label for low volume products that have ramped up.
-// etykieta dla produktów Zombie Reborn - można zmienić sobie nazwę.
-var LABEL_RAMPED_UP = 'product_ramped_up';
-
-
-// ilość kliknięć od której produkty są Zoombie lub Zombie reborn
-var THRESHOLD = '30';
-
-// LIMIT KONWERSJI, od którego produkt staje się bestsellerem
+// DOLNY PRÓG KONWERSJI, po którym produkt jest uznawany za bestseller
 var BESTSELLER_FLOOR = "50"
-// WAŻNE:
-// fitr uznający produkty za Zoombie
-var FILTER_BESTSELLERS = `metrics.conversions < ${BESTSELLER_FLOOR}` ;
 
-// Tego nie zmieniamy
-var FILTER_RAMPED_UP = 'segments.product_custom_attribute' + CUSTOM_LABEL_NR + ' = "' + LABEL_LOW + '" ';
-
-// Jesli jest true to uruchomione jest filtrowanie danych na podstawie jednej kampanii
-// nazwę kampanii należy podać w "Twoja nazwa kampanii"
+//KAMPANIA - nazwa lub wszystkie
 var USE_CAMPAIGN_FILTER = true;
 var FILTER_CAMPAIGN_NAME = ' AND campaign.name LIKE "PLA Smart" ';
 
@@ -51,6 +28,18 @@ var TIME_DURATION = 'LAST_30_DAYS';
 
 // Ilość przetwarzanych produktów
 var COUNT_LIMIT = '100000';
+// <----------------------------------------------------------USTAWIENIA_KONIEC---------------------------------------------------------->
+
+
+
+
+// Tego nie zmieniamy
+var FILTER_RAMPED_UP = 'segments.product_custom_attribute' + CUSTOM_LABEL_NR + ' = "' + LABEL_BESTSELLERS + '" ';
+var FILTER_BESTSELLERS = `metrics.conversions > ${BESTSELLER_FLOOR}` ;
+
+// Jesli jest true to uruchomione jest filtrowanie danych na podstawie jednej kampanii
+// nazwę kampanii należy podać w "Twoja nazwa kampanii"
+
 
 
 function main() {
@@ -58,15 +47,11 @@ function main() {
   // do tego są potrzezbne zakłądki Report ALL, Report RAMPED_UP, Report LOW
   //getReport('metrics.clicks < 40', 'Report ALL');
   //getReport('segments.product_custom_attribute' + CUSTOM_LABEL_NR + ' = "' + LABEL_RAMPED_UP + '" ', 'Report RAMPED_UP');
-  //getReport('segments.product_custom_attribute' + CUSTOM_LABEL_NR + ' = "' + LABEL_LOW + '" ', 'Report LOW');
+  //getReport('segments.product_custom_attribute' + CUSTOM_LABEL_NR + ' = "' + LABEL_BESTSELLERS + '" ', 'Report LOW');
 
   
-  Logger.log('Wszystkich oznaczonych produktów LABEL_LOW z warunku FILTER_ALL');
+  Logger.log('Wszystkich oznaczonych produktów LABEL_BESTSELLERS z warunku FILTER_ALL');
 	var productsAll       = getFilteredShoppingProducts(FILTER_BESTSELLERS, 'ALL');
-  Logger.log('Wszystkich oznaczonych produktów LABEL_RAMPED_UP z warunku FILTER_RAMPED_UP');
-	var productsRampedUp  = getFilteredShoppingProducts(FILTER_RAMPED_UP, 'Produkty RAMPED_UP');
-	var products = productsAll.concat(productsRampedUp);
-	pushToSpreadsheet(products);
 }
 
 function getFilteredShoppingProducts(filters, ReportName) {
@@ -105,14 +90,11 @@ function getFilteredShoppingProducts(filters, ReportName) {
 		var productId = row['segments.product_item_id']
     var label = row['segments.product_custom_attribute4']
 
-		// Label product as LABEL_LOW
-		if (label != LABEL_LOW && conversions > BESTSELLER_FLOOR) {
-			products.push([productId.toUpperCase(), LABEL_LOW]);
+		// Label product as LABEL_BESTSELLERS
+		if (label != LABEL_BESTSELLERS && conversions > BESTSELLER_FLOOR) {
+			products.push([productId.toUpperCase(), LABEL_BESTSELLERS]);
 			count += 1;
 		} 
-    // Label product as ramped up
-    // WAŻNE
-    // Warunek Zombie reborn
 	}
 	Logger.log(count);
 	return products;
